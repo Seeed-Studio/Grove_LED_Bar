021a14ac455c3cf0194c91cb4caf72df2f08e905
王晨 <46109055+PowerfulCat@users.noreply.github.com>
2019-07-09 16:22:33 +0800

    use device enumeration type

--- examples/V3_Level/V3_Level.ino
+++ examples/V3_Level/V3_Level.ino
@@ -1,11 +1,16 @@
 /*
 Grove LED Bar - Brightness Level Example
 ...
+Ported for MSP-EXP430F5529, TM4c123 (Tiva C) LaunchPad By Cameron P. LaFollette
 */
 
 #include <Grove_LED_Bar.h>
 
-Grove_LED_Bar bar(9, 8, 0);  // Clock pin, Data pin, Orientation
+// Arduino Clock pin, Data pin, Orientation 
+// Grove_LED_Bar bar(9, 8, 0); // Clock pin, Data pin, Orientation
+
+// LaunchPad Clock pin, Data pin, Orientation
+Grove_LED_Bar bar(35, 36, 0);  // Clock pin, Data pin, Orientation
 
 void setup()
 {
