59b904e04f892c1803bacf4b3c226b5803791704
王晨 <46109055+PowerfulCat@users.noreply.github.com>
2019-07-09 16:23:00 +0800

    use device enumeration type

--- examples/V3_Wave/V3_Wave.ino
+++ examples/V3_Wave/V3_Wave.ino
@@ -1,11 +1,16 @@
 /*
 Grove LED Bar - Wave Example
 ...
+Ported for MSP-EXP430F5529, TM4C123 (Tiva C) by Cameron P. LaFollette
 */
 
 #include <Grove_LED_Bar.h>
 
-Grove_LED_Bar bar(9, 8, 0);  // Clock pin, Data pin, Orientation
+// Arduino Clock pin, Data pin, Orientation 
+// Grove_LED_Bar bar(9, 8, 0); // Clock pin, Data pin, Orientation
+
+// LaunchPad Clock pin, Data pin, Orientation
+Grove_LED_Bar bar(35, 36, 0);  // Clock pin, Data pin, Orientation
 
 void setup()
 {
@@ -45,8 +50,8 @@ unsigned char state[] =
 
 void loop()
 {
-  for (byte i = 0; i < (sizeof(state) - 9); i++) {
-    bar.setBits(state + i);
+  for (byte i = 0; i < (sizeof(state)-9); i++) {
+    bar.setBits((unsigned int)state[i]);
     delay(50);
   };
 }
