833caac03d60fce6a8e611910575897b062560c4
王晨 <46109055+PowerfulCat@users.noreply.github.com>
2019-07-09 16:23:21 +0800

    use device enumeration type

--- examples/Walk/Walk.ino
+++ examples/Walk/Walk.ino
@@ -3,11 +3,18 @@ Grove LED Bar - Walk Example
 This example will show you how to use setBits() function of this library.
 Set any combination of LEDs using 10 bits.
 This example walks through all 1024 (2^10) possible combinations.
+
+Ported for MSP-EXP430F5529, TM4c123 (Tiva C) LaunchPad By Cameron P. LaFollette
 */
 
 #include <Grove_LED_Bar.h>
 
-Grove_LED_Bar bar(9, 8, 0);  // Clock pin, Data pin, Orientation
+// Arduino Clock pin, Data pin, Orientation 
+// Grove_LED_Bar bar(9, 8, 0); // Clock pin, Data pin, Orientation
+
+// LaunchPad Clock pin, Data pin, Orientation
+Grove_LED_Bar bar(35, 36, 0);  // Clock pin, Data pin, Orientation
+
 
 void setup()
 {
